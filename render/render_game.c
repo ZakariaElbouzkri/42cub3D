428b4020ac9ae5f173c1e4e84a22357768516e23
Zakaria El Bouzkri <zel-bouz@e2r9p9.1337.ma>
2023-11-10 16:13:51 +0100

    raycasting

--- render/render_game.c
+++ render/render_game.c
@@ -6,7 +6,7 @@
 /*   By: zel-bouz <zel-bouz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 03:20:27 by zel-bouz          #+#    #+#             */
-/*   Updated: 2023/11/08 00:37:44 by zel-bouz         ###   ########.fr       */
+/*   Updated: 2023/11/10 15:12:29 by zel-bouz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,28 @@ void keypress(void *ptr)
 	rend = (t_render *)ptr;
 	if (mlx_is_key_down(rend->mlx, MLX_KEY_ESCAPE))
 		clear_t_render(rend, NULL);
-	const int x = mlx_is_key_down(rend->mlx, MLX_KEY_A) * -SPEED + mlx_is_key_down(rend->mlx, MLX_KEY_D) * SPEED;
-	const int y = mlx_is_key_down(rend->mlx, MLX_KEY_W) * -SPEED + mlx_is_key_down(rend->mlx, MLX_KEY_S) * SPEED;
+	int x = mlx_is_key_down(rend->mlx, MLX_KEY_A) * -SPEED + mlx_is_key_down(rend->mlx, MLX_KEY_D) * SPEED;
+	int y = mlx_is_key_down(rend->mlx, MLX_KEY_W) * -SPEED + mlx_is_key_down(rend->mlx, MLX_KEY_S) * SPEED;
 	const double rot = mlx_is_key_down(rend->mlx, MLX_KEY_LEFT) * -0.05 + mlx_is_key_down(rend->mlx, MLX_KEY_RIGHT) * 0.05;
 	rend->player.angle += rot;
-	if (x != 0)
-		rend->player.x += x;
+	if (x != 0){
+		if (rend->map[(int)(rend->player.y / TAIL)][(int)((rend->player.x + x) / TAIL)] != '1')
+			rend->player.x += x;
+	}
 	else if (y != 0)
 	{
-		rend->player.x += cos(rend->player.angle) * ((y < 0) * SPEED + (y > 0) * -SPEED);
-		rend->player.y += sin(rend->player.angle) * ((y < 0) * SPEED + (y > 0) * -SPEED);
+		x = cos(rend->player.angle) * ((y < 0) * SPEED + (y > 0) * -SPEED);
+		y = sin(rend->player.angle) * ((y < 0) * SPEED + (y > 0) * -SPEED);
+		if (rend->map[(int)((rend->player.y + y) / TAIL)][(int)((rend->player.x + x) / TAIL)] != '1' ||
+			rend->map[(int)((rend->player.y + y + 1) / TAIL)][(int)((rend->player.x + x) / TAIL)] != '1' ||
+			rend->map[(int)((rend->player.y + y) / TAIL)][(int)((rend->player.x + x + 1) / TAIL)] != '1')
+		{
+			rend->player.x += x;
+			rend->player.y += y;
+		}
 	}
 	mlx_delete_image(rend->mlx, rend->image);
-	rend->image = mlx_new_image(rend->mlx, rend->width, rend->height);
+	rend->image = mlx_new_image(rend->mlx, WIDTH, HEIGHT);
 	mlx_image_to_window(rend->mlx, rend->image, 0, 0);
 	draw_map(rend);
 }
@@ -80,10 +89,10 @@ void	clear_t_render(t_render *rend, const char *err)
 
 void	init_mlx_vars(t_render *rend)
 {
-	rend->mlx = mlx_init(rend->width, rend->height, "CUB3D", true);
+	rend->mlx = mlx_init(WIDTH, HEIGHT, "CUB3D", true);
 	if (!rend->mlx)
 		clear_t_render(rend, mlx_strerror(mlx_errno));
-	rend->image = mlx_new_image(rend->mlx, rend->width, rend->height);
+	rend->image = mlx_new_image(rend->mlx, WIDTH, HEIGHT);
 	if (!rend->image || mlx_image_to_window(rend->mlx, rend->image, 0, 0) < 0)
 		clear_t_render(rend, mlx_strerror(mlx_errno));
 	if (!load_textures(rend))
@@ -95,10 +104,10 @@ void	init_mlx_vars(t_render *rend)
 
 void	render_game(t_render *render)
 {
-	for (int i=0; render->map[i]; i++)
-	{
-		printf("%s\n", render->map[i]);
-	}
+	// for (int i=0; render->map[i]; i++)
+	// {
+	// 	printf("%s\n", render->map[i]);
+	// }
 	// exit(0);
 	init_mlx_vars(render);
 	// //----------Drawing function----------
