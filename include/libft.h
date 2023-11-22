b7ded84c4187f716470feb3948e863ebf3874172
Zakaria El bouzkri <elbouzkri9@gmail.com>
2023-11-22 09:40:11 +0100

    get redoff some unused function in lib

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: zel-bouz <zel-bouz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 02:26:09 by zel-bouz          #+#    #+#             */
-/*   Updated: 2023/11/22 03:13:05 by zel-bouz         ###   ########.fr       */
+/*   Updated: 2023/11/22 09:37:35 by zel-bouz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,57 +28,21 @@
 # endif
 
 void	*ft_memset(void *b, int c, size_t len);
-void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_calloc(size_t count, size_t size);
 void	ft_putstr_fd(char *s, int fd);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putnbr_fd(int n, int fd);
-void	ft_putendl_fd(char *s, int fd);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_strchr(const char *s, char c);
-char	*ft_strjoin_std(char const *s1, char const *s2);
 char	*ft_strrchr(const char *s, int c);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strdup(const char *src);
 char	*ft_strtrim(const char *str, const char *set);
-char	*ft_itoa(int n);
 char	**ft_split(char const *s, char c);
-void	ft_striteri(char *s, void (*f)(unsigned int, char*));
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int		ft_strcmp(char *s1, char *s2);
-// *****
-char	*ft_strjoin_std(char const *s1, char const *s2);
-char	*ft_strcpy(char *dst, const char *src);
-char	*ft_strndup(const char *s1, size_t len);
-void	ft_swap(int *a, int *b);
-// *****
 size_t	ft_strlen(const char *s);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-// *******
-int		ft_isalpha(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-int		ft_isprint(int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-int		ft_memcmp(const void *str1, const void *str2, size_t n);
 int		ft_atoi(const char *str);
 int		ft_max(int a, int b);
 bool	ft_empty(const char *str);
-// ft_printf functions :
-int		ft_printf(const char *format, ...);
-int		ft_putchar(char c);
-int		ft_putnbr(int n);
-int		ft_putstr(char *s);
-int		ft_puthexa(unsigned long n, char c);
-int		ft_putunbr(unsigned int n);
-int		ft_putptr(unsigned long n);
 void	ft_puterror(int n, ...);
-// Get_next_line :
 char	*get_next_line(int fd);
 
 #endif
--- lib/ft_strtrim.c
+++ lib/ft_strtrim.c
@@ -6,12 +6,11 @@
 /*   By: zel-bouz <zel-bouz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 21:22:22 by zel-bouz          #+#    #+#             */
-/*   Updated: 2023/11/07 23:34:19 by zel-bouz         ###   ########.fr       */
+/*   Updated: 2023/11/22 09:39:05 by zel-bouz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
-
 
 char	*ft_strtrim(const char *str, const char *set)
 {
@@ -23,7 +22,7 @@ char	*ft_strtrim(const char *str, const char *set)
 	if (!*str)
 		return (ft_strdup(""));
 	start = 0;
-	end	  = ft_strlen(str) - 1;
+	end = ft_strlen(str) - 1;
 	while (start <= end && ft_strchr(set, str[start]))
 		start++;
 	while (end && ft_strrchr(set, str[end]))
